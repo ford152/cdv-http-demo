2f4fc8dc6ea2928e4f5af72bde02c45ee82cb59b
Alex Shore <ford152@yahoo.com>
2017-09-20 11:41:27 -0600

    testing

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,34 @@
 const app = {
-    acceptsAllCerts: false,
+  acceptsAllCerts: false,
 
-    // Application Constructor
-    initialize: function() {
-        document.getElementById('toggleAllCertsBtn').addEventListener('click', this.onToggleAllCertsBtnClick.bind(this));
-        document.getElementById('getBtn').addEventListener('click', this.onGetBtnClick.bind(this));
-    },
+  // Application Constructor
+  initialize: function () {
+    document.getElementById('toggleAllCertsBtn').addEventListener('click', this.onToggleAllCertsBtnClick.bind(this));
+    document.getElementById('getBtn').addEventListener('click', this.onGetBtnClick.bind(this));
+    document.getElementById('clearBtn').addEventListener('click', this.onClearBtnClick.bind(this));
+  },
 
-    print: function(content) {
-      document.getElementById('resultTextarea').value += '\n' + JSON.stringify(content);
-    },
+  print: function (content) {
+    document.getElementById('resultTextarea').value += '\n' + JSON.stringify(content);
+  },
 
-    onGetBtnClick: function() {
-      const url = document.getElementById('urlInput').value;
+  onGetBtnClick: function () {
+    const url = document.getElementById('urlInput').value;
 
-      cordova.plugin.http.get(url, {}, {}, data => this.print(data), error => this.print(error));
-    },
+    cordova.plugin.http.get(url, {}, { Host: "www.bitnrg.io" }, data => this.print(data), error => this.print(error));
+  },
 
-    onToggleAllCertsBtnClick: function() {
-      const accept = this.acceptsAllCerts = !this.acceptsAllCerts;
+  onClearBtnClick: function () {
+    document.getElementById('resultTextarea').value = "";
+  },
 
-      cordova.plugin.http.acceptAllCerts(accept, () => this.print(`${accept ? '' : 'not '}accepting all certs`));
-      cordova.plugin.http.validateDomainName(!accept, () => this.print(`${accept ? 'not ' : ''}validating domain name`));
-    }
+  onToggleAllCertsBtnClick: function () {
+    const accept = this.acceptsAllCerts = !this.acceptsAllCerts;
+
+    cordova.plugin.http.acceptAllCerts(accept, () => this.print(`${accept ? '' : 'not '}accepting all certs`));
+    cordova.plugin.http.validateDomainName(!accept, () => this.print(`${accept ? 'not ' : ''}validating domain name`));
+    cordova.plugin.http.enableSSLPinning(!accept, () => this.print(`${accept ? 'not ' : ''}performing ssl pinning`));
+  }
 };
 
 app.initialize();
